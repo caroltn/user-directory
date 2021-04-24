9066f424fb665f102cc39b7afc12e37f17a592c1
Carol Nguyen <cthoa01@gmail.com>
2021-04-24 13:29:07 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,49 +1,19 @@
-import { useState, useEffect } from 'react'
-import ReactTable from 'react-table-v6'
-import axios from 'axios'
-import 'react-table-v6/react-table.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import MainContainer from './components/MainContainer'
+import './components/style.css'
 
-const App = () => {
-  const [userState, setUserState] = useState({
-    users: [],
-    columns: [
-      {
-        Header: 'name',
-        accessor: 'name',
-        filterable: true
-      },
-      {
-        Header: 'email',
-        accessor: 'email',
-        filterable: true
-      },
-      {
-        Header: 'phone',
-        accessor: 'phone',
-        filterable: true
-      }
-    ]
-  })
-
-  useEffect(() => {
-    axios.get('https://randomuser.me/api?results=20')
-      .then(({ data: { results } }) => {
-        const users = results.map(user => ({
-          name: `${user.name.first} ${user.name.last}`,
-          email: user.email,
-          phone: user.phone
-        }))
-        setUserState({ ...userState, users })
-      })
-      .catch(err => console.error(err))
-  }, [])
+function App() {
+  
 
   return (
-    <ReactTable
-      data={userState.users}
-      columns={userState.columns}
-    />
-  )
+    <div className="App">
+      <Navbar />
+      <MainContainer />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
